0f761c16afcbac2d273700e853fbfc7cd187b980
Postea <ydb1997@hotmail.de>
2022-11-20 19:38:04 +0100

    Added two example cpp-files

---
+++ src/lib.cpp
@@ -0,0 +1,8 @@
+// Example Lib
+
+#include <iostream>
+
+int hallo() {
+    std::cout << "Hello World!";
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// Your First C++ Program
+
+#include <iostream>
+#include "lib.cpp"
+
+int main() {
+    hallo();
+    std::cout << "Hello World!";
+    return 0;
+}
